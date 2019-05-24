7aedbd5e0f4d5dd39c55f29b3234e43550cae90f
divvelavasavimounika <50195810+divvelavasavimounika@users.noreply.github.com>
2019-05-24 12:48:19 +0530

    Updated Model

---
+++ model/Product.js
@@ -0,0 +1,12 @@
+module.exports = class Product{
+    constructor(id, description, finish, price,status, imagesList){
+        this.id = id;
+        this.description = description;
+        this.finish = finish;
+        this.price = price;
+        this.status = status;
+        this.imagesList = imagesList;
+
+    }
+
+}
