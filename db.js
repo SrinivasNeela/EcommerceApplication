031d9f54bc47a5da2011d71f4edc9456d24eba18
divvelavasavimounika <50195810+divvelavasavimounika@users.noreply.github.com>
2019-05-24 12:40:06 +0530

    Updated

---
+++ db.js
@@ -0,0 +1,30 @@
+const MongoClient = require('mongodb').MongoClient
+
+const state = {
+  db: null,
+}
+exports.connect = (url, done) => {
+  if (state.db) return done()
+  MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+    if (err) { console.log(err); return done(err)}
+    state.db = client.db('ecommerce')
+    done()
+  })
+}
+
+exports.getCollection = (collectionName) => {
+    return state.db.collection(collectionName)
+};
+
+exports.getDB = () => state.db
+
+exports.close = (done) => 
+{
+  if (state.db) {
+    state.db.close((err, result) => {
+      state.db = null
+     state.mode = null
+      done(err)
+    })
+  }
+}
